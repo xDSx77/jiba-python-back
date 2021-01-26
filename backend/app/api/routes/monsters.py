01b9399a57269f68ed46caf2cae70f2d239c4383
Doomky <james.richet@epita.fr>
2021-01-26 18:02:45 +0100

    Added MonsterService and Updated Monster

--- backend/app/api/routes/monsters.py
+++ backend/app/api/routes/monsters.py
@@ -1,15 +1,16 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
-
+from app.api.services.monsterService import MonsterService
+from app.api.models.monster import Monster
 
 router = APIRouter()
 
 
 @router.get("/")
-async def get_all_monsters() -> object:
-    return None
+async def get_all_monsters() -> list[Monster]:
+    return MonsterService().get_all()
 
 
 @router.get("/info/{monster_id}")
 async def get_monster_info(monster_id: int) -> object:
-    return None
+    return MonsterService().get_monster_info(monster_id)
--- backend/app/api/services/monsterService.py
+++ backend/app/api/services/monsterService.py
@@ -1,4 +1,14 @@
+from app.db.repositories.monsterRepository import MonsterRepository
+from app.db.repositories.monsterTypeRepository import MonsterTypeRepository
+
 class MonsterService:
 
     def __init__(self):
+        self.monster_repository = MonsterRepository()
+        self.monster_type_repository = MonsterTypeRepository()
+
+    def get_all(self):
+        return self.monster_repository.get_all()
+
+    def get_monster_info(self, monster_id: int):
         pass
