3261e7686a683ae621cf24e78ff796d18a9fb02f
Doomky <james.richet@epita.fr>
2021-01-26 11:29:48 +0100

    Initialize project with Dockerfile, docker-compose and server.py for fast API

---
+++ backend/app/api/server.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+def get_application():
+    app = FastAPI(title="Phresh", version="1.0.0")
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    return app
+app = get_application()
