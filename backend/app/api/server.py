29e4c548e021379c6560f5c50a805a2f21ec886d
Doomky <james.richet@epita.fr>
2021-01-26 12:39:40 +0100

    Added api routes, tasks for db and app config

--- backend/app/api/server.py
+++ backend/app/api/server.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from app.core import config, tasks
+from app.api.routes import router as api_router
 
 
 def get_application():
-    app = FastAPI(title="python-rpg-api", version="1.0.0")
+    app = FastAPI(title=config.PROJECT_NAME, version=config.VERSION)
     app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
@@ -11,6 +13,11 @@ def get_application():
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+    app.add_event_handler("startup", tasks.create_start_app_handler(app))
+    app.add_event_handler("shutdown", tasks.create_stop_app_handler(app))
+    app.include_router(api_router, prefix="/api")
+
     return app
 
 
