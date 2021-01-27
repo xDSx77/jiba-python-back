76e5ea2180e4228914c715a730a90e6f8b314bd9
Bastien Seira <bastien.seira@epita.fr>
2021-01-27 11:56:38 +0100

    Add rest + attack to playerService

--- backend/app/api/services/playerService.py
+++ backend/app/api/services/playerService.py
@@ -1,12 +1,17 @@
 from typing import List
+from app.db.models.monster import Monster
 from app.db.models.player import Player
 from app.db.repositories import PlayerRepository
+from app.db.repositories import MonsterInfoRepository
+from app.db.repositories import MonsterRepository
 
 
 class PlayerService:
 
     def __init__(self):
         self.player_repository = PlayerRepository()
+        self.monster_info_repository = MonsterInfoRepository()
+        self.monster_repository = MonsterRepository()
 
     def get_all_players(self) -> List[Player]:
         return self.player_repository.get_all()
@@ -22,7 +27,60 @@ class PlayerService:
         return self.player_repository.get_by_username(username)
 
     def rest(self, username: str) -> Player:
-        pass
+        player = self.player_repository.get_by_username(username)
+        updated_player = Player(player.username,
+                                player.level,
+                                player.xp,
+                                player.xp_max,
+                                max(player.hp + 5, player.hp_max),
+                                player.hp_max,
+                                player.gold)
+        self.player_repository.update_by_username(updated_player)
+        return self.player_repository.get_by_username(username)
 
     def attack(self, username: str, monster_id: int) -> object:
-        pass
+        player = self.player_repository.get_by_username(username)
+        monster_info = self.monster_info_repository.get_by_id(monster_id)
+        updated_monster = Monster(monster_id,
+                                  max(monster_info.hp - player.level, 0))
+        updated_player_xp = player.xp
+        updated_player_level = player.level
+        updated_player_xp_max = player.xp_max
+        updated_hp_max = player.hp_max
+        updated_hp = player.hp
+        updated_gold = player.gold
+
+        if updated_monster.hp <= 0:
+            updated_player_xp += monster_info.xp_value
+            updated_gold += monster_info.gold_value
+        else:
+            updated_hp = max(player.hp - monster_info.damage, 0)
+
+        if updated_player_xp >= player.xp_max:
+            updated_player_level += 1
+            updated_player_xp -= player.xp_max
+            updated_player_xp_max *= 1.8
+            updated_hp_max += 5
+            updated_hp = updated_hp_max
+
+        if updated_hp <= 0:
+            updated_hp = updated_hp_max
+            updated_gold = int(player.gold/2)
+
+        updated_player = Player(player.username,
+                                updated_player_level,
+                                updated_player_xp,
+                                updated_player_xp_max,
+                                updated_gold,
+                                updated_hp_max,
+                                player.gold + monster_info.gold_value)
+        if updated_monster.hp <= 0:
+            self.monster_repository.delete_by_id(updated_monster.id)
+        else:
+            self.monster_repository.update_by_id(updated_monster)
+        self.player_repository.update_by_username(updated_player)
+
+        return {
+            "dead": updated_player.hp <= 0,
+            "killed": monster_info.hp <= 0
+        }
