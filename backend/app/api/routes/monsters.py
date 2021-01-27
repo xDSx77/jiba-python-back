3dc80d0dd8edfeb3ca763e186c133f7620e1786a
Allan Cantin <allan.cantin@epita.fr>
2021-01-27 11:47:40 +0100

    Updated monsters route with return types + added implementation for get_monster_info of MonsterService

--- backend/app/api/routes/monsters.py
+++ backend/app/api/routes/monsters.py
@@ -1,5 +1,6 @@
+from typing import List
+
 from fastapi import APIRouter
-from pydantic import BaseModel
 from app.api.services.monsterService import MonsterService
 from app.db.models.monster import Monster
 
@@ -7,10 +8,10 @@ router = APIRouter()
 
 
 @router.get("/")
-async def get_all_monsters() -> list[Monster]:
+async def get_all_monsters() -> List[Monster]:
     return MonsterService().get_all()
 
 
 @router.get("/info/{monster_id}")
-async def get_monster_info(monster_id: int) -> object:
+async def get_monster_info(monster_id: int) -> Monster:
     return MonsterService().get_monster_info(monster_id)
--- backend/app/api/services/monsterService.py
+++ backend/app/api/services/monsterService.py
@@ -1,3 +1,6 @@
+from typing import List
+
+from app.db.models.monster import Monster
 from app.db.repositories import MonsterRepository
 from app.db.repositories import MonsterTypeRepository
 
@@ -8,8 +11,8 @@ class MonsterService:
         self.monster_repository = MonsterRepository()
         self.monster_type_repository = MonsterTypeRepository()
 
-    def get_all(self):
+    def get_all(self) -> List[Monster]:
         return self.monster_repository.get_all()
 
-    def get_monster_info(self, monster_id: int):
-        pass
+    def get_monster_info(self, monster_id: int) -> Monster:
+        return self.monster_repository.get_by_monster_id(monster_id)
